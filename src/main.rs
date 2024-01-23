1d26b6ece4f3191033d3393f8b038f559975e3c7
jjsuperpower <jjs29356@gmail.com>
2024-01-22 20:35:05 -0600

    adding support for umount on shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use fuse_mt;
-use log::debug;
-// use std::{io::{self, prelude::*}, fs, os::unix::prelude::FileExt};
+use log::{debug, info};
+use signal_hook::iterator::Signals;
+use signal_hook::consts::{SIGINT,SIGTERM};
+use std::process::Command;
+use std::thread;
+use ctrlc;
 
 mod crypt_fs;
-
 use crypt_fs::CryptFS;
 
 struct ConsoleLogger;
-
+static LOGGER: ConsoleLogger = ConsoleLogger;
 impl log::Log for ConsoleLogger {
     fn enabled(&self, _metadata: &log::Metadata<'_>) -> bool {
         true
@@ -19,8 +22,6 @@ impl log::Log for ConsoleLogger {
 
     fn flush(&self) {}
 }
-
-static LOGGER: ConsoleLogger = ConsoleLogger;
 
 
 // main function
@@ -35,8 +36,22 @@ fn main() -> std::io::Result<()> {
     let key = "012345689abcdefg";
     let crypt_fs = CryptFS::new(String::from(key), String::from(src_path), None);
 
-    // print!("{}", crypt_fs.get_real_path(Path::new("/test.txt")).display());
-    fuse_mt::mount(fuse_mt::FuseMT::new(crypt_fs, 1), &mnt_path, &[])?;
+    // let mut signals = Signals::new(&[SIGINT,SIGTERM])?;
+    // thread::spawn(move || {
+    //     for sig in signals.forever() {
+    //         debug!("Received signal {:?}", sig);
+    //         info!("Unmounting and exiting");
+    //         Command::new("umount").arg(mnt_path).spawn().expect("Error unmounting");
+    //         break;
+    //     }
+    // });
+
+    ctrlc::set_handler(move || {
+        info!("Unmounting and exiting");
+        Command::new("umount").arg(mnt_path).spawn().expect("Error unmounting");
+    }).expect("Error setting Ctrl-C handler");
+
+    fuse_mt::mount(fuse_mt::FuseMT::new(crypt_fs, 1), mnt_path, &[])?;
 
     return Ok(());
 }
